221d251a87dbf46105ba2c60e828d63ec047dd1d
lewis <lxx0103@yahoo.com>
2020-01-14 02:34:09 +0800

    Update config.php

--- vinos_core/apps/config/config.php
+++ vinos_core/apps/config/config.php
@@ -32,7 +32,7 @@ $config['base_url'] = 'http://test.com';
 |
 | Typically this will be your index.php file, unless you've renamed it to
 | something else. If you are using mod_rewrite to remove the page set this
-| variable so that it is blank.
+| variable so that it is blank.tttt
 |
 */
 $config['index_page'] = '';
