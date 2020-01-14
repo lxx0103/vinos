3e9cd05b59956556242545af47bd58427718ad2d
lewis <lxx0103@yahoo.com>
2020-01-15 04:42:59 +0800

    a

--- admin/vinos_core/views/home_v.php
+++
@@ -1,6 +0,0 @@
-<?php require_once(VIEWPATH.'common/head_v.php');?>
-  <div class="container-fluid">
-  </div>
-</div>
-<?php require_once(VIEWPATH.'common/footer_v.php');?>
-<?php require_once(VIEWPATH.'common/foot_v.php');?>
